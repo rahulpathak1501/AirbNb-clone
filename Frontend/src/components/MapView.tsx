536d16fa94a5f59d384e30835437a2c61d730096
rahulpathak1501 <rahul150101130015@gmail.com>
2025-08-12 18:46:51 +0530

    adding Mapview

---
+++ Frontend/src/components/MapView.tsx
@@ -0,0 +1,36 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Property } from "../types/Property";
+import "leaflet/dist/leaflet.css";
+import { LatLngExpression } from "leaflet";
+
+interface MapViewProps {
+  properties: Property[];
+}
+
+const MapView: React.FC<MapViewProps> = ({ properties }) => {
+  const center: LatLngExpression = [48.8566, 2.3522]; // Default center (Paris)
+
+  return (
+    <MapContainer
+      center={center}
+      zoom={2}
+      className="w-full h-[500px] rounded-xl"
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {properties.map((property) => (
+        <Marker
+          key={property._id}
+          position={[property.latitude, property.longitude]} // Use real coordinates
+        >
+          <Popup>
+            <strong>{property.title}</strong>
+            <br />
+            {property.location}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+};
+
+export default MapView;
